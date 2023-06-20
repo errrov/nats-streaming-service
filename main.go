d6f54cbd024b3b6d96327961643910685b8e13d1
e1vo <deihp6@gmail.com>
2023-06-20 20:13:47 +0300

    Created basic server with graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello world")
+	l := log.New(os.Stdout, "nats-streaming service ", log.LstdFlags)
+	server := &http.Server{
+		Addr:     ":8080",
+		ErrorLog: l,
+	}
+
+	go func() {
+		l.Println("Started server on port :8080")
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			l.Println("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	signal.Notify(quit, os.Kill)
+
+	sig := <-quit
+	log.Println("Got signal : %v", sig)
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(ctx)
+
 }
