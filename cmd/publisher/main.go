08448ffe9d7366be0d65e6fb4bcf6efd4c2ee515
e1vo <deihp6@gmail.com>
2023-06-22 12:15:15 +0300

    Created basic realisation of pub/sub

--- cmd/publisher/main.go
+++ cmd/publisher/main.go
@@ -1,1 +1,63 @@
 package main
+
+import (
+	"encoding/json"
+	"log"
+	"reflect"
+	"wildberries_L0/internal/broker"
+
+	"fmt"
+	"os"
+	"wildberries_L0/internal/model"
+)
+
+func main() {
+	sc, err := broker.ConnectToNats("test-cluster", "order-publisher")
+	if err != nil {
+		log.Fatalf("Error connecting to nats %v", err)
+	}
+	defer sc.Close()
+	modelJSON, err := os.ReadFile("model.json")
+	if err != nil {
+		log.Fatalf("Error reading json: %v", err)
+	}
+	var sendOrder model.Order
+	err = json.Unmarshal(modelJSON, &sendOrder)
+	if err != nil {
+		log.Fatalf("Error unmarshaling JSON: %v", err)
+	}
+	fmt.Println(sendOrder)
+
+	sendOrder.OrderUID = "ABCDTESTING123"
+	sendingData, err := json.Marshal(sendOrder)
+	if err != nil {
+		log.Fatalf("Error marshalling order %v", err)
+	}
+	fmt.Println(reflect.TypeOf(sendingData))
+	if err = sc.Publish("testing", sendingData); err != nil {
+		log.Fatalf("Error sending Order %v", err)
+	}
+
+	/*
+		modelJSON, err := os.ReadFile("model.json")
+		if err != nil {
+			log.Println(err)
+		}
+		var order model.Order
+		err = json.Unmarshal(modelJSON, &order)
+		if err != nil {
+			log.Println(err)
+		}
+		order.OrderUID = "TESTING"
+		data, err := json.Marshal(order)
+		if err != nil {
+			log.Println(err)
+		}
+
+		if err := pb.Publish("test-test", data); err != nil {
+			log.Println(err)
+		}
+
+		log.Printf("Published order with UID = %v", order.OrderUID)
+	*/
+}
--- cmd/subscriber/main.go
+++ cmd/subscriber/main.go
@@ -1,45 +1,64 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
-	"syscall"
-	"time"
+	"reflect"
+
+	"github.com/nats-io/stan.go"
 )
 
 func main() {
-	fmt.Println("Hello world")
-	l := log.New(os.Stdout, "subscriber part ", log.LstdFlags)
-	server := &http.Server{
-		Addr:     ":8080",
-		ErrorLog: l,
+	sc, err := stan.Connect("test-cluster", "order-subscriber", stan.NatsURL(stan.DefaultNatsURL))
+	if err != nil {
+		log.Println(err)
 	}
-
+	log.Printf("Connected")
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan bool)
+	signal.Notify(signalChan, os.Interrupt)
 	go func() {
-		l.Println("Started server on port :8080")
-		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			l.Printf("Error starting server: %s\n", err)
-			os.Exit(1)
+		for range signalChan {
+			fmt.Printf("\nReceived an interrupt, unsubscribing and closing connection...\n\n")
+			sc.Close()
+			cleanupDone <- true
 		}
 	}()
+	_, _ = sc.Subscribe("testing", func(msg *stan.Msg) {
+		if err := msg.Ack(); err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(string(msg.Data))
+		log.Println(reflect.TypeOf(msg.Data))
+	}, stan.SetManualAckMode())
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
-	signal.Notify(quit, syscall.SIGTERM)
+	<-cleanupDone
+	/*
+		fmt.Println("Hello world")
+		srv := server.NewServer()
+		go func() {
+			srv.Server.ErrorLog.Println("Started server on port :8080")
+			if err := srv.Server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				srv.Server.ErrorLog.Printf("Error starting server: %s\n", err)
+				os.Exit(1)
+			}
+		}()
 
-	sig := <-quit
-	log.Printf("Got signal : %v", sig)
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, os.Interrupt)
+		signal.Notify(quit, syscall.SIGTERM)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
+		sig := <-quit
+		log.Printf("Got signal : %v", sig)
 
-	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("Server shutdown failed: %v\n", err)
-	}
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
 
+		if err := srv.Server.Shutdown(ctx); err != nil {
+			log.Fatalf("Server shutdown failed: %v\n", err)
+		}
+	*/
 }
